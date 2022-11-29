55826a516b13628ca6803d57e7ce56887c42f50a
javi <haldrim377@gmail.com>
2022-11-29 19:15:38 -0400

    Commit 16

--- frontend/src/forms/variedad/FormVariedad.js
+++ frontend/src/forms/variedad/FormVariedad.js
@@ -1,6 +1,7 @@
 import { Form, Button } from 'react-bootstrap'
 import axios from 'axios'
 import { useState } from 'react'
+import backstyle from './backstyle.css'
 
 export default function FormVariedad(){
 
@@ -31,47 +32,52 @@ export default function FormVariedad(){
 
     return(
         <>
-             <div className='container mt-xxl-5 d-flex align-items-center justify-content-center'>
-                <Form className='w-100' onSubmit= {handleSubmit}>
-                    <Form.Group controlId='formNombre'>
-                        <Form.Label>Nombre</Form.Label>
-                            <Form.Control type='text' name='nombre' onChange={handleChange}/>
-                    </Form.Group>
-                    <Form.Group controlId='formEspecie'>
-                        <Form.Label>Especie</Form.Label>
-                            <Form.Select type='text' name='especie' defaultValue='Prunus' onChange={handleChange}>
-                                <option disabled>Selecciona una opcion</option>
-                                <option>Prunus avium</option>
-                                <option>Prunus cerasus</option>
-                            </Form.Select>
-                    </Form.Group>
-                    <Form.Group controlId='formPrecocidad'>
-                        <Form.Label>Precocidad</Form.Label>
-                            <Form.Select type='text' name='preco' defaultValue='Temprana' onChange={handleChange}>
-                                <option disabled>Selecciona una opcion</option>
-                                <option>Temprana</option>
-                                <option>Tardia</option>
-                            </Form.Select>
-                    </Form.Group>
-                    <Form.Group controlId='formDescripcion'>
-                        <Form.Label>Descripcion</Form.Label>
-                            <Form.Group className=' w-100' as='textarea' rows={3} name='descrip' onChange={handleChange}>
-                            </Form.Group>
-                    </Form.Group>
-                    <Form.Group controlId='formPaisOri'>
-                        <Form.Label>Pais Origen</Form.Label>
-                            <Form.Select type='text' name='paisorigen' defaultValue='id_pais' onChange={handleChange}>
-                                <option disabled>Selecciona una opcion</option>
-                                <option>Espana</option>
-                                <option>Francia</option>
-                                <option>Chile</option>
-                            </Form.Select>
-                    </Form.Group>
-                    <Button className='mt-4'variant="primary" type="submit">
-                            Submit
-                    </Button>
-                </Form>
-             </div>
+            <div className=' d-flex align-items-center justify-content-center w-100 h-100'>
+                <div className='container back'>
+                    <Form className='w-100 text-white d-flex flex-column justify-content-between p-5 gap-3' onSubmit= {handleSubmit}>
+                        <h1 className=' text-center'>Registrar Variedad</h1>
+                        <Form.Group>
+                            <Form.Label className=' fs-5'>Nombre</Form.Label>
+                                <Form.Control className='rounded-0 border' type='text' name='nombre' onChange={handleChange}/>
+                        </Form.Group>
+                        <Form.Group>
+                            <Form.Label className=' fs-5'>Especie</Form.Label>
+                                <Form.Select className='rounded-0 border' name='especie' defaultValue='Prunus' onChange={handleChange}>
+                                    <option disabled>Selecciona una opcion</option>
+                                    <option>Prunus avium</option>
+                                    <option>Prunus cerasus</option>
+                                </Form.Select>
+                        </Form.Group>
+                        <Form.Group>
+                            <Form.Label className=' fs-5'>Precocidad</Form.Label>
+                                <Form.Select className='rounded-0 border' name='preco' defaultValue='Temprana' onChange={handleChange}>
+                                    <option disabled>Selecciona una opcion</option>
+                                    <option>Temprana</option>
+                                    <option>Tardia</option>
+                                </Form.Select>
+                        </Form.Group>
+                        <Form.Group>
+                            <Form.Label className=' fs-5'>Descripcion</Form.Label>
+                                <Form.Group className=' w-100' as='textarea' rows={3} name='descrip' onChange={handleChange}>
+                                </Form.Group>
+                        </Form.Group>
+                        <Form.Group>
+                            <Form.Label className=' fs-5'>Pais Origen</Form.Label>
+                                <Form.Select className='rounded-0 border' name='paisorigen' defaultValue='id_pais' onChange={handleChange}>
+                                    <option disabled>Selecciona una opcion</option>
+                                    <option>Espana</option>
+                                    <option>Francia</option>
+                                    <option>Chile</option>
+                                </Form.Select>
+                        </Form.Group>
+                        <Form.Group className=' d-flex align-content-center justify-content-center' >
+                            <Button className=' w-25 text-black bg-light rounded-0 border border-2 border-dark' type="submit">
+                                    Registrar
+                            </Button>
+                        </Form.Group>
+                    </Form>
+                </div>
+            </div>
         </>
     )
 }
